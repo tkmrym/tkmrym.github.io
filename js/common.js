fbcaf316a9b165dd3308e5246830f20dd780a241
tkmrym <64920986+tkmrym@users.noreply.github.com>
2021-04-21 13:51:52 +0900

    Update slide show

--- js/common.js
+++ js/common.js
@@ -7,10 +7,11 @@ $(function() {
 	});
 
 $(function() {
-$('.header-img-slide img:nth-child(n+2)').hide();
+$('.header-img-slide img:nth-child(n)').hide();
+	$(".header-img-slide img:first-child").fadeIn(1500);
 	setInterval(function() {
-		$(".header-img-slide img:first-child").fadeOut(2000);
-		$(".header-img-slide img:nth-child(2)").fadeIn(2000);
+		$(".header-img-slide img:first-child").fadeOut(1500);
+		$(".header-img-slide img:nth-child(2)").fadeIn(1500);
 		$(".header-img-slide img:first-child").appendTo(".header-img-slide");
-	}, 4000);
+	}, 13000);
 });
