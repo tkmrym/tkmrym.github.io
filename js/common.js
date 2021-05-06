65ce87b527673fe8a728d38a9f2e3cca49d7efe4
tkmrym <64920986+tkmrym@users.noreply.github.com>
2021-05-06 15:14:46 +0900

    Delete fadein effect

--- js/common.js
+++ js/common.js
@@ -15,21 +15,3 @@ $('.header-img-slide img:nth-child(n)').hide();
 		$(".header-img-slide img:first-child").appendTo(".header-img-slide");
 	}, 13000);
 });
-
-$(function(){
-	$(window).on('load scroll',function (){
-		$('.main-fadein').each(function(){
-			//ターゲットの位置を取得
-			var target = $(this).offset().top;
-			//スクロール量を取得
-			var scroll = $(window).scrollTop();
-			//ウィンドウの高さを取得
-			var height = $(window).height();
-			//ターゲットまでスクロールするとフェードインする
-			if (scroll > target - height){
-				//クラスを付与
-				$(this).addClass('main-fadein-active');
-			}
-		});
-	});
-});
